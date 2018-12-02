e67bbecfa167f3022a190f0fc87aaf23e3ecb6ab
Sahil Sharma <sahilsharma@cmail.carleton.ca>
2018-12-02 18:13:45 -0500

    restore old code, previous commit consisted non functional code

--- main.js
+++ main.js
@@ -18,14 +18,6 @@ app.get('/getData', function(req, res){
 
 		var db = client.db("aqua");
 
-		db.posts.find({}, {_id : 1})
-    		.limit(5)
-   		    .sort({timestamp:1})
-    		.toArray()
-    		.map(function(doc) { return doc._id; });
-
-		db.posts.remove({_id: {$in: removeIdsArray}})
-
 		db.collection('posts').find().limit(1).sort({$natural:-1}).toArray(function(err, result){
 			if(err){
 				console.log(err);
